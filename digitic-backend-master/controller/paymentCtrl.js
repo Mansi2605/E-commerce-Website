f5230aa982826a785e437794369aefcfc7f22e0a
Mansi Kapshe <mansikapshe@gmail.com>
2023-04-23 17:19:29 +0530

    Committing Backend

---
+++ digitic-backend-master/controller/paymentCtrl.js
@@ -0,0 +1,27 @@
+const Razorpay = require("razorpay")
+const instance = new Razorpay({
+    key_id:"rzp_te",key_secret:"GK0drgO"
+})
+
+const checkout = async (req, res) => {
+    const {amount}=req.body
+    const option = {
+        amount: amount * 100 ,
+        currency:"INR"
+    }
+    const order = await instance.orders.create(option)
+    res.json({
+        success: true,
+        order
+    })
+}
+const paymentVerification = async (req, res) => {
+    const { razorpayOrderId, razorpayPaymentId } = req.body
+    res.json({
+        razorpayOrderId,razorpayPaymentId
+    })
+}
+
+module.exports = {
+    checkout,paymentVerification
+}
